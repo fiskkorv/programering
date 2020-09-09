66f1764eeadd3d37c870e54378ddb9a95531b698
Kalander <alwinkalander08@gmail.com>
2020-09-09 16:10:30 +0200

    personer till äpplen 3

---
+++ uppgift_4.py
@@ -0,0 +1,16 @@
+def division(äpplen,personer):
+    if personer==0:
+        print("försök inte")
+        return
+    else:
+        return int(äpplen/personer)
+
+personer=int(input("skriv in hur många personer "))
+äpplen=int(input("skriv in hur många äpplen "))
+äpplen_per_person=division(äpplen,personer)
+print("äpplen per person " + str(äpplen_per_person))
+
+utdelade_äpplen = äpplen_per_person * personer
+äpplen_kvar = äpplen % personer
+
+print("äpplen kvar " + str(äpplen_kvar))
